6317c879bdcde0a84e7279e06f05cc746a7ec6b1
RSS Francisco <paco666@outlook.com>
2016-12-08 03:35:20 -0600

    Add files via upload

---
+++ Presentacion/Consultas/empleadoEntre.xaml.cs
@@ -0,0 +1,37 @@
+﻿#region Regiones
+using System.Collections.Generic;
+using System.Windows;
+using Negocios;
+#endregion
+
+namespace Presentacion.Consultas
+{
+    /// <summary>
+    /// Interaction logic for empleadoEntre.xaml
+    /// </summary>
+    public partial class empleadoEntre : Window
+    {
+        #region Atributos
+
+        RegistroEmpleado _registroEmpleado = new RegistroEmpleado();
+        List<Empleado> misEmpleados = null;
+        //Empleado _EmpleadoActual = null;
+        #endregion
+        public empleadoEntre()
+        {
+            InitializeComponent();
+            misEmpleados = _registroEmpleado.Listar();
+        }
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+            misEmpleados = _registroEmpleado.ListarDescendente();
+            this.Close();
+        }
+    }
+}
---
+++ Presentacion/Helper/Helper.cs
@@ -0,0 +1,21 @@
+﻿using Negocios;
+
+namespace Presentacion
+{
+  public  class Helper
+    {
+        Core _objCore = new Core();
+        public int ObtenerNumeroFilas(string llavePrimaria, string tabla,int tamanio)
+        {
+            int numeroFilas = _objCore.ObtenerNumeroFilas(llavePrimaria,tabla);
+            int numeroPaginas = numeroFilas / tamanio;
+            return numeroPaginas; 
+        }
+        // Metodo que actualiza el estado de un producto en el stock de la base de datos del sistema
+        //public bool actualizar_existencia_producto(string llavePrimaria, string accion, int nuevaCantidad, int cantidadAnterior)
+        //{
+        //    return _objCore.actualizar_existencia_producto(llavePrimaria, accion, nuevaCantidad, cantidadAnterior);
+        //}
+       
+    }
+}
